4a2738499845345366b3e485f5b36c172e5e6f13
Dina Riad <dinayriad@gmail.com>
2020-02-09 18:04:59 +0200

    Create_order is added

--- controllers/orders.js
+++ controllers/orders.js
@@ -2,56 +2,65 @@ var mongo = require('mongodb');
 var mongoose=require('mongoose')
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://nayera:1234@cluster0-dwvui.mongodb.net";
-
 const dbName = "Fattarny";
 const tag = "Controller_Orders";
 
-exports.get_user_orders = (req, res) => {
-    console.log(tag + "-get_user_orders-" + res);
-    var userId = parseInt(req.params.user_id);
-    
+exports.get_orders_history = (req, res) => {
+    var userId = req.params.user_id;
+
     MongoClient.connect(url, function(err, db) {
-        console.log(tag + "-mongo_connection-error" + err);
-         if (err) throw err;
+         if (err) res.status(500).send("Bad Connection");
+
          const dbo = db.db(dbName);
-         var query = {user_id : userId};
+         var query = {user_id : userId, is_paid : true};
          dbo.collection("Orders").find(query).toArray(function(err, orders) {
-            console.log(tag + "-get_user_orders-error-" + err);
-            if (err) throw err;
+            if (err) res.status(404).send("Not found");
             res.status(200).send(orders);
-            db.close();
-          });   
-        });
-}
-
-exports.set_paid = (req, res) => {
-    console.log(tag + "-set_paid-" + res);
-    var orderId = parseInt(req.params.order_id);
-
-    
-    MongoClient.connect(url, function(err, db) {
-        console.log(tag + "-mongo_connection-error" + err);
-         if (err) throw err;
-         const dbo = db.db(dbName);
-
-        var query = {id : orderId};
-        var newValue = {$set: {is_paid : true}};
-
-         dbo.collection("Orders").updateOne(query, newValue, function(err, orders) {
-            console.log(tag + "-set_paid-error-" + err);
-            if (err) throw err;
-            res.status(200).send(orders);
+            
             db.close();
         });   
     });
 }
 
-//Create new order.
+exports.set_is_paid = (req, res) => {
+    var orderId = req.params.order_id;
+    var isPaid =  JSON.parse(req.params.is_paid);
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) res.status(500).send("Bad Connection");
+         
+        const dbo = db.db(dbName);
+        var query = {_id : mongoose.Types.ObjectId(orderId)};
+        var newValue = {$set: {is_paid : isPaid}};
+
+         dbo.collection("Orders").updateOne(query, newValue, function(err, response) {
+            if (err) res.status(404).send("Not found");
+            res.status(200).send(response);
+            
+            db.close();
+        });   
+    });
+}
+
+exports.create_order = (req, res) => {
+    var newOrder = req.body;
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) res.status(500).send("Bad Connection");
+         
+        const dbo = db.db(dbName);
+         dbo.collection("Orders").insertOne(newOrder, function(err, response) {
+            if (err) res.status(404).send("Not found");
+            res.status(200).send(response);
+            
+            db.close();
+        });   
+    });
+}
+
 //get all paid orders for today.
 //get all orders for today.
 
-
-//Cancel order.
 exports.cancel_order = (req, res) => {
     console.log(tag + "-cancel_order-" + res);
     var orderId = (req.params.order_id);
--- routes/orders.js
+++ routes/orders.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 const OrdersController= require('../controllers/orders');
 
-router.get("/:user_id", OrdersController.get_user_orders);
-router.put("/set_paid_for/:order_id", OrdersController.set_paid);
+router.get("/:user_id", OrdersController.get_orders_history);
+router.put("/set_is_paid/:order_id/:is_paid", OrdersController.set_is_paid);
 router.delete("/cancel_order/:order_id",OrdersController.cancel_order);
+router.post("/create_order", OrdersController.create_order);
 module.exports = router;
