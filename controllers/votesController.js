d32ca863840430d979a0027ab1e8b2c04ca05d1b
NayeraMohamed <32566231+NayeraMohamed@users.noreply.github.com>
2020-02-10 13:01:53 +0200

    Update votesController.js

--- controllers/votesController.js
+++ controllers/votesController.js
@@ -2,7 +2,7 @@ const mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://nayera:1234@cluster0-dwvui.mongodb.net";
 
-exports.addVote = function(req, serverRes) {
+exports.addVote = function(req, res) {
     MongoClient.connect(url, function(err, db) {
         if (err) //connection error
         {
@@ -13,14 +13,14 @@ exports.addVote = function(req, serverRes) {
             user_id: req.body.user_id,
             id: req.body.id,
             date: req.body.date};
-        dbo.collection("votesHistory").insertOne(newVote, function(err, res) {
+        dbo.collection("votesHistory").insertOne(newVote, function(err, vote) {
           if (err)
           {
             res.status(500).send("Weak Internet Connection");
           }
           else
           {
-            serverRes.status(200).send("Okay");
+            res.status(200).send("Okay");
           }         
           db.close();
         });
